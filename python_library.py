377d03f6a9deb3a0ed9fcabcee42155c7fb7fb44
Dave Pcolar <dave@dpn.org>
2019-01-20 14:10:01 -0500

    Initial commit

---
+++ python_library.py
@@ -0,0 +1,110 @@
+"""DPN Python Library"""
+__copyright__ = "Copyright (C) 2017 Digital Preservation Network, LLC"
+__license__ = "BSD Version 3 License"
+
+
+import json
+import csv
+import datetime
+import time
+import os
+import boto3
+from botocore.exceptions import ClientError
+import requests
+
+# Note: create the credentials file in .aws before using
+#
+
+def log_message(message):
+    """print out  in log message format"""
+    print "%s:  %s" % (datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), message)
+
+def log_json_message(log_message):
+    """print out  in json tagged log message format"""
+    log_message['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    print json.dumps(log_message)
+    log_message={}
+
+def loggily_json_message(log_message):
+    """Push message to Loggily in json tagged log message format"""
+    log_message['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    payload=json.dumps(log_message)
+    response = requests.post("http://logs-01.loggly.com/inputs/5cadf10d-100f-4a85-90b3-e69cbe6fc8e1/tag/http/", data=payload)
+    if response.status != 200:
+        print response
+    log_message={}
+
+def  create_csv(json_file):
+    """create a csv file from the json data"""
+    output_filename = json_file[:json_file.find('json')] + "csv"
+    log_message("File created: " + output_filename)
+    output_file = open(output_filename, 'w')
+    csvwriter = csv.writer(output_file)
+    first_row = True
+    try:
+      with open(json_file) as input_file:
+          json_data = json.load(input_file)
+          for keys in json_data.keys():
+              records = json_data[keys]
+      rec_num = 0
+      while rec_num < len(records):
+          record = records[rec_num]
+          if first_row:
+              csvwriter.writerow(record.keys())
+              first_row = False
+          values = record.values()
+        # csvwriter is ascii only - change encoding
+          values = [char.encode(encoding='ascii', errors='replace') for char in values]
+          csvwriter.writerow(values)
+          rec_num += 1
+    except ClientError as boto3_error:
+      log_message("ERROR: File " + json_file + " not found")
+      log_message(boto3_error.message)
+    else:
+      log_message("Rows exported: " + str(len(records)))
+      input_file.close()
+
+    output_file.close()
+
+def download_s3_file(json_file, s3_bucket='dpn-dcv'):
+    """ dowload an object from a s3 bucket """
+    if json_file:
+        # connect to the S3 service
+        s3 = boto3.resource('s3')
+        # download the object
+        try:
+          s3.Object(s3_bucket, json_file).download_file(json_file)
+        except ClientError as boto3_error:
+          log_message("ERROR: S3 object not found " + json_file)
+          log_message(boto3_error.message)
+
+def delete_s3_file(file_object, s3_bucket='dpn-dcv'):
+    """  Delete a file object from S3 """
+    if file_object:
+	# connect to the S3 service and
+        # delete the object
+        client = boto3.client('s3')
+        client.delete_object(Bucket=s3_bucket, Key=file_object)
+        log_message("Object Deleted: " + file_object)
+
+def get_uuidv4():
+    """ Call service for UUID v4  """
+    response = requests.get('https://www.uuidgenerator.net/api/version4')
+    if response.status_code is 200:
+      uuid=r.text
+      log_message("UUID: " + uuid)
+    return uuid
+
+def load_from_environment(return_args, *argv):
+# read content from environment variables
+# environment arg names are passed as strings and returned in a dictionary.
+# not found is returned as a null string
+
+    for arg in argv:
+       if  arg in os.environ:
+          return_args[arg] = os.environ[arg]
+#         print arg + " value is: " + return_args[arg]
+       else:
+          return_args[arg] = None
+    return(0)
+
---
+++ test.py
@@ -0,0 +1,25 @@
+# First step is to import the necessary libraries.
+# Ex: import requests
+# What libraries do you need to accomplish this task?
+
+
+
+
+
+
+# Next up we need to make the API call to retrieve the data
+
+
+
+
+
+
+# Once we have the data, we need to get it in the format we want
+# Think about the end product, we need a CSV file that clearly displays the data to the user
+
+
+
+
+
+
+# Once we have the data where we want it, we need to write the CSV file
